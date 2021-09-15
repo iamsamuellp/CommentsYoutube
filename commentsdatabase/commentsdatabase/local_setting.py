99914f7773223e1ba61f1da69d005e278aba85ec
Samuel McKnight <samuellp@gmail.comgit statusgit config --global user.user.email iamsamuellp@gmail.com>
2021-09-15 14:48:41 -0500

    added django to workspace and connecting sql to the workspace

---
+++ commentsdatabase/commentsdatabase/local_setting.py
@@ -0,0 +1,26 @@
+try:
+  from commentsdatabase.local_setting import *
+except ImportError:
+  pass
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-xngvlz6u^fndts3m^5qj(gl90rh9a1((j*o+n77($m$0c63yln'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'comments_database',
+        'USER': 'root',
+        'PASSWORD':'Thispassword1',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}

4431cac8eef98567a457b68db5e73a451dd39b25
Samuel <iamsamuellp@gmail.com>
2021-09-19 12:23:48 -0500

    added changes to the models py for comments , and migrated  those changes to sql. Also started adding reply to the Comments Youtbe

--- commentsdatabase/comments/models.py
+++ commentsdatabase/comments/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Comments(models.Model):
-    first = models.CharField(max_length=50)
-    last = models.CharField(max_length=50)
+    video_Id = models.CharField(max_length=100, blank=True, null=True)
+    likes = models.IntegerField(default= 0)
+    dislikes = models.IntegerField(default= 0)
     text = models.CharField(max_length=300)

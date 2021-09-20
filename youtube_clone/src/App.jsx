40266b898095b9c72136504e3b28acf7f2d94ea5
Samuel <iamsamuellp@gmail.com>
2021-09-20 14:01:52 -0500

    fixed app.jsx problem where return was deleted making render undefined

--- youtube_clone/src/App.jsx
+++ youtube_clone/src/App.jsx
@@ -1,7 +1,7 @@
 import Search from './Components/Searchbar/searchbar.jsx';
 import axios from 'axios';
 import React, { Component } from 'react';
-import youtubeAPIKey from './APIKEY';
+//import youtubeAPIKey from './APIKEY';
 
 
 class App extends Component {
@@ -9,23 +9,30 @@ class App extends Component {
     super(props);
     this.state = { 
 
-     }}
-  
-  }
-  
-  
-  handleSubmit = async (searchTerm) => {
-    try{
-      let query = "https://www.googleapis.com/youtube/v3/search?q=" + searchTerm +"&key=" + youtubeAPIKey + "&part=snippet&type=video";
-      const result = await axios.get(query)
-      this.setState({videoList: result.data.items})
+     }
     }
-    catch (ex){
-      console.log("Can't get video: " + ex)
-    }
-  }
-  render ()
-
+  
+  
+  
+  
+  // handleSubmit = async (searchTerm) => {
+  //   try{
+  //     let query = "https://www.googleapis.com/youtube/v3/search?q=" + searchTerm +"&key=" + youtubeAPIKey + "&part=snippet&type=video";
+  //     const result = await axios.get(query)
+  //     this.setState({videoList: result.data.items})
+  //   }
+  //   catch (ex){
+  //     console.log("Can't get video: " + ex)
+  //   }
+  // }
+  render () {
+    return (
+      <div>
+        <h1>Hello World </h1>
+      </div>
+    );
+  } 
+}
 
 export default App;
 
